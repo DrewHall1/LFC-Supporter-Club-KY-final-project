14c10ebc4beb92ace9996cf2ed0e68432facfe8d
WILLIAM HALL <williamhall@WILLIAMs-MacBook-Pro.local>
2017-07-21 08:51:19 -0400

    first commit of final project

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+/* --- CAROUSEL ---*/
+
+$('.carousel').carousel();
+
+
+
+function myFunction() {
+    document.getElementById("frm1").addEventListener("submit", myFunction);
+    	alert("Thanks you for your interest! Keep your eyes open for an email with more information about our group.");
+    
+  }
+
